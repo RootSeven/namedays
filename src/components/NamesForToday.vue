159e8de718973d6835ffde40b6f2e52ebf2e8d8f
RootSeven <mairifairbairn@gmail.com>
2020-04-03 18:27:11 +0100

    Adds numberToMonth array, starts formatting into table

--- src/components/NamesForToday.vue
+++ src/components/NamesForToday.vue
@@ -3,7 +3,17 @@
       <h3>Today's date is</h3>
       <h2>{{ nameDayToday.dates.day }}/{{ nameDayToday.dates.month }}</h2>
       <h3>and the names for today are</h3>
-      <h2 v-for="(name, country) in nameDayToday.namedays" :value="name" :key="country">{{ countryCodes[country.toString()] }}: {{ name }}</h2>
+      <table>
+        <tr>
+          <th>Country</th>
+          <th>Names</th>
+        </tr>
+        <tr>
+          <td>Germany</td>
+          <td>Dorothy</td>
+        </tr>
+      </table>
+      <!-- <h2 v-for="(name, country) in nameDayToday.namedays" :value="name" :key="country">{{ countryCodes[country.toString()] }}: {{ name }}</h2> -->
   </div>
 </template>
 
@@ -15,5 +25,7 @@ export default {
 </script>
 
 <style scoped>
-
+  table {
+    width: 100%;
+  }
 </style>

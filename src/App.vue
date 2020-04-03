198648dc9a27294810fe21e53716de6f62f8bda8
RootSeven <mairifairbairn@gmail.com>
2020-04-03 12:35:19 +0100

    Can get Today's Names from API

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <h1>Name Days</h1>
+        <names-for-today :nameDayToday = "nameDayToday"></names-for-today>
     <h2>What is a Name Day?</h2>
     <p>A name day is a day of the year associated with a certain given name, and these days are celebrated in some parts of Europe and Latin America. These names are related to saints in Roman Catholicism and Eastern Orthodox Christianity. The day is celebrated in much the same way as one's birthday.</p>
   </div>
 </template>
 
 <script>
+import NamesForToday from './components/NamesForToday.vue';
+
 export default {
   name: "app",
 
@@ -21,9 +24,15 @@ export default {
     .then( response => response.json() )
     .then( nameDayToday => this.nameDayToday = nameDayToday );
   },
+
+  components: {
+    "names-for-today": NamesForToday
+  }
 }
 </script>
 
 <style scoped>
-
+  div {
+    text-align: center;
+  }
 </style>
---
+++ src/components/NamesForToday.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <h3>Today's date is</h3>
+      <h2>{{ nameDayToday.data.dates.day }}/{{ nameDayToday.data.dates.month }}</h2>
+      <h3>and the names for today are</h3>
+      <h2 v-for="(value, key) in nameDayToday.data.namedays">{{ value }}</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'names-for-today',
+    props: ['nameDayToday']
+
+}
+</script>
+
+<style scoped>
+
+</style>
